47c3cc92535f3301b0b2fc97507ac329185c3c61
hans4dev <16078890+hans4dev@users.noreply.github.com>
2024-03-13 11:33:30 +0100

    docs: start README, offload meta-info research

---
+++ README.md
@@ -0,0 +1,17 @@
+## Meta information about a threat modeling activity
+
+<quote>
+Information identifying the threat model typically includes the following:
+1. Application Name: The name of the application examined.
+2. Application Version: The version of the application examined.
+3. Description: A high level description of the application.
+4. Document Owner: The owner of the threat modeling document.
+5. Participants: The participants involved in the threat modeling process for this application.
+6. Reviewer: The reviewer(s) of the threat model.
+</quote>
+
+Source: OWASP Foundation (2023-Nov): [Threat Modeling Process, Threat Model Information](https://owasp.org/www-community/Threat_Modeling_Process#threat-model-information).
+
+See also:
+* Blog "Let's Talk About MedSec" (24 Apr 2022): [Threat Modeling Knowledge Bases and Templates](https://tmart234.github.io/threat-model-template/)
+* Repo from [Izar Tarandach](https://owasp.org/www-board-candidates/2023/izar_tarandach): [izar/pytm](https://github.com/izar/pytm) : A Pythonic framework for threat modeling
--- confluence_collector.py
+++ confluence_collector.py
@@ -9,32 +9,15 @@ from model import ConductionOfSimpleThreatModelingOnTechnicalLevel, \
     ConductionOfSimpleThreatModelingOnTechnicalLevelComponent, Link
 
 """
-This example finds pages with specific labels.
+This example finds  Confluence wiki pages with specific labels.
 * https://github.com/atlassian-api/atlassian-python-api/tree/master/examples/confluence
 
 To collect meta-information for threat models:
-* Team
+* Team or Application
 * Title
 * Date
 * Participants
 * Issues (e.g. links to JIRA)
-
-
-<quote>
-Information identifying the threat model typically includes the the following:
-1. Application Name: The name of the application examined.
-2. Application Version: The version of the application examined.
-3. Description: A high level description of the application.
-4. Document Owner: The owner of the threat modeling document.
-5. Participants: The participants involved in the threat modeling process for this application.
-6. Reviewer: The reviewer(s) of the threat model.
-</quote>
-
-Source: OWASP Foundation (2023-Nov): [Threat Modeling Process, Threat Model Information](https://owasp.org/www-community/Threat_Modeling_Process#threat-model-information).
-
-See also: 
-* Blog "Let's Talk About MedSec" (24 Apr 2022): [Threat Modeling Knowledge Bases and Templates](https://tmart234.github.io/threat-model-template/)
-* Repo from [Izar Tarandach](https://owasp.org/www-board-candidates/2023/izar_tarandach): [izar/pytm](https://github.com/izar/pytm) : A Pythonic framework for threat modeling
 """
 
 CONFLUENCE_URL = environ.get("CONFLUENCE_URL")  # example: "https://example.atlassian.net/wiki"
